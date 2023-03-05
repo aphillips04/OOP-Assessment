2e13b051dbff25b20639b68609e817cff69766fb
Alex Phillips <2004alexphillips@gmail.com>
2023-03-05 17:40:32 +0000

    implement card class

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -12,7 +12,50 @@ namespace CMP1903M_A01_2223
         //Value: numbers 1 - 13
         //Suit: numbers 1 - 4
         //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
+        private int _value;
+        private int _suit;
+
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (Value >= 1 && Value <= 13)
+                {
+                    _value = value;
+                }
+                else
+                {
+                    throw new Exception("Value must be between 1 and 13");
+                }
+            }
+        }
+        public int Suit
+        {
+            get
+            {
+                return _suit;
+            }
+            set
+            {
+                if (Suit >= 1 && Suit <= 4)
+                {
+                    _suit = value;
+                }
+                else
+                {
+                    throw new Exception("Suit must be between 1 and 4");
+                }
+            }
+        }
+
+        public Card(int value, int suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
     }
 }
